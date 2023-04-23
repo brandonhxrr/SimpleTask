d2e2b6e6572aba09d8e23ae9cae40d09a38c6a4a
Brandon Herrera <brandondh769@gmail.com>
2023-04-23 11:43:34 -0600

    Migrate to Material3

--- app/src/main/java/com/brandonhxrr/simpletask/MainActivity.kt
+++ app/src/main/java/com/brandonhxrr/simpletask/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
             SimpleTaskTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
 
                 }
