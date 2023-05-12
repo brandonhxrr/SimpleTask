daba366c188cf5d3de42a25877e7c51009e5cad9
Brandon Herrera <brandondh769@gmail.com>
2023-05-12 14:40:07 -0600

    Add ScrollableTabRow and tabs

--- app/src/main/java/com/brandonhxrr/simpletask/ui/TasksScreen.kt
+++ app/src/main/java/com/brandonhxrr/simpletask/ui/TasksScreen.kt
@@ -7,13 +7,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.brandonhxrr.simpletask.R
@@ -59,7 +65,8 @@ fun TaskScreen() {
                             Text(
                                 text = stringResource(id = R.string.app_name),
                                 style = MaterialTheme.typography.headlineMedium,
-                                modifier = Modifier.align(Alignment.CenterVertically)
+                                modifier = Modifier.align(Alignment.CenterVertically),
+                                color = MaterialTheme.colorScheme.onBackground
                             )
                         }
                     }
@@ -75,21 +82,40 @@ fun TaskScreen() {
             )
         },
         content = {
-
+            var state by remember { mutableStateOf(0) }
+            val titles = listOf("Tab 1", "Tab 2", "Tab 3 with lots of text")
             Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(top = it.calculateTopPadding()),
             ) {
-                Spacer(modifier = Modifier.height(24.dp))
-
-                Text(
-                    text = "Tareas",
-                    style = MaterialTheme.typography.titleMedium,
-                    modifier = Modifier
-                        .padding(horizontal = 32.dp),
-                    color = Color(0xFF7286D3)
-                )
+                ScrollableTabRow(selectedTabIndex = state, edgePadding = 0.dp) {
+                    titles.forEachIndexed { index, title ->
+                        Tab(
+                            selected = state == index,
+                            onClick = { state = index },
+                            text = { Text(text = title, maxLines = 2, overflow = TextOverflow.Ellipsis) },
+                            selectedContentColor = MaterialTheme.colorScheme.primary,
+                            unselectedContentColor = MaterialTheme.colorScheme.onBackground
+                        )
+                    }
+                    Tab(
+                        selected = state == titles.size,
+                        onClick = {state = titles.size},
+                        text = {
+                            Row{
+                                Icon(
+                                imageVector = Icons.Filled.Add,
+                                contentDescription = "Localized description"
+                                )
+                                Spacer(modifier = Modifier.width(4.dp))
+                                Text(text = "Nueva tarea", modifier = Modifier.align(Alignment.CenterVertically))
+                            }
+                               },
+                        selectedContentColor = MaterialTheme.colorScheme.primary,
+                        unselectedContentColor = MaterialTheme.colorScheme.onBackground
+                    )
+                }
             }
         }
     )
