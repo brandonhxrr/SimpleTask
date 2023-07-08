9ab8e22818b5f41adf28afc9b49bfd6cd18eac2f
Brandon Herrera <brandondh769@gmail.com>
2023-07-07 18:31:56 -0600

    Fix tab divider

--- app/src/main/java/com/brandonhxrr/simpletask/ui/TasksScreen.kt
+++ app/src/main/java/com/brandonhxrr/simpletask/ui/TasksScreen.kt
@@ -11,12 +11,15 @@ import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.with
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -25,10 +28,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.brandonhxrr.simpletask.R
@@ -104,7 +111,10 @@ fun TaskScreen(
                     .fillMaxSize()
                     .padding(top = padding.calculateTopPadding()),
             ) {
-                ScrollableTabRow(selectedTabIndex = taskUiState.selectedTab, edgePadding = 0.dp, modifier = Modifier.fillMaxWidth()) {
+                ScrollableTabRow(selectedTabIndex = taskUiState.selectedTab, edgePadding = 0.dp, modifier = Modifier
+                    .fillMaxWidth()
+                    .shadow(2.dp),
+                divider = { Divider(color = MaterialTheme.colorScheme.background)}) {
                     taskUiState.taskList.forEachIndexed { index, taskList ->
                         Tab(
                             selected = taskUiState.selectedTab == index,
@@ -131,6 +141,7 @@ fun TaskScreen(
                         unselectedContentColor = MaterialTheme.colorScheme.onBackground
                     )
                 }
+                Spacer(modifier = Modifier.height(8.dp))
                 AnimatedContent(
                     targetState = taskUiState.selectedTab,
                     transitionSpec = {
@@ -141,16 +152,13 @@ fun TaskScreen(
                         targetOffsetX = { -it },
                         animationSpec = tween(durationMillis = 500, easing = FastOutSlowInEasing)
                     )
-                }) {tab ->
-                    println("Selected tab: $tab")
-                    println("Task list size: ${taskUiState.taskList.size}")
-                    TabContent(taskUiState.taskList[tab].tasks)
+                }) {
+                        tab -> TabContent(taskUiState.taskList[tab].tasks)
                 }
             }
         }
     )
 }
-
 
 @Composable
 fun TabContent(tabItems: List<Task>){
